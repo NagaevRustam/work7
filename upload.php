0689fe0858909a9c9cb13f882cee0cbfc777f77e
NagaevRustam <nagaevrustam@yandex.ru>
2024-12-18 17:12:27 +0300

    Change version

--- upload.php
+++ upload.php
@@ -1,16 +1,27 @@
 <?php
-session_start();
-// Проверяем, существует ли уже счетчик посещений
-if (!isset($_SESSION['count'])) {
-    $_SESSION['count'] = 0;
-}
-// Увеличиваем счетчик посещений
-$_SESSION['count']++;
-// Проверка на кратность посещения третьей страницы
-if ($_SESSION['count'] % 3 == 0) {
-//    header('Location: <file_name class="html"></file_name>');
-    header('Location: file_name.html');
+$file_name = $_POST['file_name'];
+$content = $_FILE['content'];
+
+if (!empty($file_name)) { //Проверка на заполнение названия файла
+    if (!empty($content)) { //Проверка на то, что файл выбран
+        $uploaddir = 'C:\Program Files\Ampps\www';
+        $uploadfile = $uploaddir . basename($_FILES['content']['name']);
+        
+        if (move_uploaded_file($_FILES['content']['tmp_name'], $uploadfile)){
+            echo 'Файл корректен и был успешно загружен.' . PHP_EOL;
+        } else {
+            echo 'Возможно атака с помощью файловой загрузки!' . PHP_EOL;
+        }
+        echo '<pre>';
+        echo 'Некоторая отладочная информация'; 
+        echo '<pre>';
+        var_dump($_FILES);
+        echo '<pre>';
+    } else {
+        header("Location: index.html"); //Если файл отсутствует, то редирект на index.html
+        exit();
+    }
+} else {
+    header("Location: index.html"); // Если поле названия не заполнено , то редирект на index.html
     exit();
 }
-echo "<p>Вы посещали третью страницу ".$_SESSION['count']." раз(а).</p>";
-?>
